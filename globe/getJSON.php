66a4286e7c15eda791fe8d0ad51bf252378b9bdc
Adam Booth <adam@ambooth.com>
2012-08-04 01:32:55 -0500

    Finished main work on getJSON function
    It now receives JSON info and converts it to an assoc array for manipulation. Outputs updated json for use with the globe.

--- globe/getJSON.php
+++ globe/getJSON.php
@@ -1,29 +1,47 @@
 <?php
+
 require_once 'link_JSON.php';
 
 class GetJSON {
-    
-    private function parseJSON($url = null)
-    {
-        if ($url != null)
-        {
-            $jsonurl = jsonurl; // URL should be changed in link_JSON.php
-            $json = file_get_contents($jsonurl, 0, null, null);
-            $json_output = json_decode($json);
 
-            foreach ($json_output->trends as $trend)
+    public function parseJSON()
+    {
+        $jsonurl = JSONURL; // URL should be changed in link_JSON.php
+        $json = file_get_contents($jsonurl, 0, null, null);
+        $json_output = json_decode($json, true);
+
+        $json_output = $json_output['points'];
+        $json_output = $json_output['point'];
+
+        header('Content-type: application/json');
+
+        $first = TRUE;
+        $count = 0;
+
+
+        echo '[[[';
+        foreach ($json_output as $index => $j)
+        {
+            if (!$j['ip']) // Eleminate IP data
             {
-                echo "{$trend->name}\n";
+                if (!$first) // Prevents comma at beginning
+                {
+                    echo ',';
+                }
+                
+                echo $j['latitude'] . ',';
+                echo $j['longitude'] . ',';
+                echo '0.03';
+
+                $count++;
+
+                $first = FALSE;
             }
         }
-        else
-        {
-            echo 'No URL provided';
-        }
+        echo ']]]';
     }
 
 }
 
-GetJSON::yo();
-
+GetJSON::parseJSON();
 ?>
