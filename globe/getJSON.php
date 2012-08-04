9d26680f2d162acf7fdb3aa8321fc2c5558b2d86
Adam Booth <adam@ambooth.com>
2012-08-03 19:56:01 -0500

    Add parseJSON class

--- globe/getJSON.php
+++ globe/getJSON.php
@@ -1,36 +1,29 @@
 <?php
 require_once 'link_JSON.php';
 
-//class GetJSON {
-//    
-//    public function yo()
-//    {
-//        echo json-url;
-//    }
-//
-//    private function parseJSON($url = null)
-//    {
-//        if ($url != null)
-//        {
-//            $jsonurl = "http://it-stats.lifechurch.tv/?action=kaseya_map";
-//            $json = file_get_contents($jsonurl, 0, null, null);
-//            $json_output = json_decode($json);
-//
-//            foreach ($json_output->trends as $trend)
-//            {
-//                echo "{$trend->name}\n";
-//            }
-//        }
-//        else
-//        {
-//            echo 'No URL provided';
-//        }
-//    }
-//
-//}
-//
-//GetJSON::yo();
+class GetJSON {
+    
+    private function parseJSON($url = null)
+    {
+        if ($url != null)
+        {
+            $jsonurl = jsonurl; // URL should be changed in link_JSON.php
+            $json = file_get_contents($jsonurl, 0, null, null);
+            $json_output = json_decode($json);
 
-echo jsonurl;
+            foreach ($json_output->trends as $trend)
+            {
+                echo "{$trend->name}\n";
+            }
+        }
+        else
+        {
+            echo 'No URL provided';
+        }
+    }
+
+}
+
+GetJSON::yo();
 
 ?>
